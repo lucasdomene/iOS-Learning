4ec03efe78693b7fb8c1822217ce66f0328bac83
Lucas Firmo <lucas.firmo@gmail.com>
2016-09-06 18:32:40 -0300

    Adding Item model

---
+++ BigNerdRanch/Homepwner/Homepwner/ItemsViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ItemsViewController.swift
+//  Homepwner
+//
+//  Created by Lucas Domene Firmo on 9/6/16.
+//  Copyright © 2016 Domene. All rights reserved.
+//
+
+import UIKit
+
+class ItemsViewController: UITableViewController {
+    
+}
