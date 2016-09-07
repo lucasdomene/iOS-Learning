d868b80e30cded73a3f94823232e2ec7f25ee16a
Lucas Firmo <lucas.firmo@gmail.com>
2016-09-07 16:32:59 -0300

    Adding detail view controller

---
+++ BigNerdRanch/Homepwner/Homepwner/DetailViewController.swift
@@ -0,0 +1,44 @@
+//
+//  DetailViewController.swift
+//  Homepwner
+//
+//  Created by Lucas Domene Firmo on 9/7/16.
+//  Copyright © 2016 Domene. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    
+    @IBOutlet var nameLabel: UITextField!
+    @IBOutlet var serialNumberField: UITextField!
+    @IBOutlet var valueField: UITextField!
+    @IBOutlet var dateLabel: UILabel!
+    
+    var item: Item!
+    
+    let numberFormatter: NSNumberFormatter = {
+        let formatter = NSNumberFormatter()
+        formatter.numberStyle = .DecimalStyle
+        formatter.minimumFractionDigits = 2
+        formatter.maximumFractionDigits = 2
+        return formatter
+    }()
+    
+    let dateFormatter: NSDateFormatter = {
+        let formatter = NSDateFormatter()
+        formatter.dateStyle = .MediumStyle
+        formatter.timeStyle = .NoStyle
+        return formatter
+    }()
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        nameLabel.text = item.name
+        serialNumberField.text = item.serialNumber
+        valueField.text = numberFormatter.stringFromNumber(item.valueInDollars)
+        dateLabel.text = dateFormatter.stringFromDate(item.dateCreated)
+    }
+    
+}
