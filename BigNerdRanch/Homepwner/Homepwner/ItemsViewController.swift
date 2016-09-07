7a9f41ebd289442546d58c1a47a671f94cd01fa5
Lucas Firmo <lucas.firmo@gmail.com>
2016-09-07 18:22:33 -0300

    Adding bar button items

--- BigNerdRanch/Homepwner/Homepwner/ItemsViewController.swift
+++ BigNerdRanch/Homepwner/Homepwner/ItemsViewController.swift
@@ -12,13 +12,14 @@ class ItemsViewController: UITableViewController {
     
     var itemStore: ItemStore!
     
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        navigationItem.leftBarButtonItem = editButtonItem()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let statusBarHeight = UIApplication.sharedApplication().statusBarFrame.height
-        let insets = UIEdgeInsets(top: statusBarHeight, left: 0, bottom: 0, right: 0)
-        tableView.contentInset = insets
-        tableView.scrollIndicatorInsets = insets
         tableView.rowHeight = UITableViewAutomaticDimension
         tableView.estimatedRowHeight = 65
     }
@@ -76,16 +77,6 @@ class ItemsViewController: UITableViewController {
         if let index = itemStore.allItems.indexOf(newItem) {
             let indexPath = NSIndexPath(forItem: index, inSection: 0)
             tableView.insertRowsAtIndexPaths([indexPath], withRowAnimation: .Automatic)
-        }
-    }
-    
-    @IBAction func toggleEditingMode(sender: AnyObject) {
-        if editing {
-            sender.setTitle("Edit", forState: .Normal)
-            setEditing(false, animated: true)
-        } else {
-            sender.setTitle("Done", forState: .Normal)
-            setEditing(true, animated: true)
         }
     }
     
