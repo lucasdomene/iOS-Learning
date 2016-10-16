3f9d6d227d23ff5a2b8a7ac436eabf136b5062bf
Lucas Firmo <lucas.firmo@gmail.com>
2016-10-16 20:37:21 -0200

    Getting current user location

--- APPCoda-Intermediate/MapKitDirectionDemo/MapKitDirectionDemo/MapViewController.swift
+++ APPCoda-Intermediate/MapKitDirectionDemo/MapKitDirectionDemo/MapViewController.swift
@@ -14,11 +14,19 @@ class MapViewController: UIViewController, MKMapViewDelegate {
     @IBOutlet weak var mapView:MKMapView!
     
     var restaurant:Restaurant!
+    let locationManager = CLLocationManager()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         mapView.delegate = self
+        
+        locationManager.requestWhenInUseAuthorization()
+        let status = CLLocationManager.authorizationStatus()
+        
+        if status == .authorizedWhenInUse {
+            mapView.showsUserLocation = true
+        }
         
         // Convert address to coordinate and annotate it on map
         let geoCoder = CLGeocoder()
@@ -48,6 +56,14 @@ class MapViewController: UIViewController, MKMapViewDelegate {
         })
 
     }
+    
+    // Route
+    
+    @IBAction func showDirection(sender: AnyObject) {
+        
+    }
+    
+    // MKMapViewDelegate
 
     func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
         let identifier = "MyPin"
@@ -75,22 +91,5 @@ class MapViewController: UIViewController, MKMapViewDelegate {
         
         return annotationView
     }
-
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
