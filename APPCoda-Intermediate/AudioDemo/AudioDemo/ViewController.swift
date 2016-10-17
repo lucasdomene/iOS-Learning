f90c2438f41a0e4fb1a65f54e7d51ce5609552e5
Lucas Firmo <lucas.firmo@gmail.com>
2016-10-17 16:59:54 -0200

    Adding Audio Recorder Project

---
+++ APPCoda-Intermediate/AudioDemo/AudioDemo/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  AudioDemo
+//
+//  Created by Simon Ng on 21/11/14.
+//  Copyright (c) 2014 AppCoda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var playButton:UIButton!
+    @IBOutlet weak var stopButton:UIButton!
+    @IBOutlet weak var recordButton:UIButton!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func play(sender: AnyObject) {
+    }
+    
+    @IBAction func stop(sender: AnyObject) {
+    }
+    
+    @IBAction func record(sender: AnyObject) {
+    }
+    
+}
+
