a922f4836161c1ac9b32db806f7e1d6944b116aa
Lucas Firmo <lucas.firmo@gmail.com>
2016-10-17 23:12:00 -0200

    Configuring qr code reader

--- APPCoda-Intermediate/QRReaderDemo/QRReaderDemo/ViewController.swift
+++ APPCoda-Intermediate/QRReaderDemo/QRReaderDemo/ViewController.swift
@@ -9,14 +9,42 @@
 import UIKit
 import AVFoundation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, AVCaptureMetadataOutputObjectsDelegate {
     
     @IBOutlet weak var messageLabel:UILabel!
 
+    var captureSession: AVCaptureSession?
+    var videoPreviewLayer: AVCaptureVideoPreviewLayer?
+    var qrCodeFrameView: UIView?
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        let captureDevice = AVCaptureDevice.defaultDevice(withMediaType: AVMediaTypeVideo)
+        
+        do  {
+            let input = try AVCaptureDeviceInput(device: captureDevice)
+            
+            captureSession = AVCaptureSession()
+            captureSession?.addInput(input)
+            
+            let captureMetadataOutput = AVCaptureMetadataOutput()
+            captureSession?.addOutput(captureMetadataOutput)
+            
+            captureMetadataOutput.setMetadataObjectsDelegate(self, queue: .main)
+            captureMetadataOutput.metadataObjectTypes = [AVMetadataObjectTypeQRCode]
+            
+            videoPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+            videoPreviewLayer?.videoGravity = AVLayerVideoGravityResizeAspectFill
+            videoPreviewLayer?.frame = view.layer.bounds
+            view.layer.addSublayer(videoPreviewLayer!)
+            view.bringSubview(toFront: messageLabel)
+            
+            captureSession?.startRunning()
+        } catch {
+            print(error)
+            return
+        }
     }
     
 }
